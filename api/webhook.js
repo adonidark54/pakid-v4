ec8c55f7e0035bfd270f1a63d25fb2aa9cd06bf5
adonidark54 <adonidark54@gmail.com>
2026-06-26 13:54:20 -0400

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,8 @@
+const Stripe = require('stripe');
+ 
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  return res.status(200).json({ received: true });
+};
