3be5457e64993e42d8745d5515507f56ece27bf2
adonidark54 <adonidark54@gmail.com>
2026-06-26 13:50:35 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PaKid from './PaKid';
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(PaKid));
